f6f5105c8f49992f3e1127ff3fcbc3671387e599
A. M. Rafinul Huq <amrh.cse@gmail.com>
2024-02-16 05:55:14 +0600

    2nd file inserted

---
+++ day-01-terminal.rb
@@ -0,0 +1,47 @@
+=begin
+
+
+-------------terminal commands--------------
+
+1. pwd (print working directory)
+2. cd (change directory)
+3. ls (list)
+4. cd .. (1 directory up)
+5. cd .\Desktop\ (go to Desktop folder)
+6. clear or c (clear the terminal screen)
+7. exit (close terminal)
+8. man command (get manual for a specific command)
+9. history (show previous entered commands)
+10. !number (run a previously entered command by number from history)
+
+---------------file management----------------
+
+11. touch file_name (create an empty file)
+12. nano file_name (open and edit text files with the nano editor)
+13. cat file_name (display contents of a file)
+14. more file_name (view contents of a longer file in parts)
+15. less file_name (same as more, but allows scrolling with arrow keys)
+16. rm -i file_name (remove a file after confirmation)
+17. cp source destination (copy a file)
+18. mv source destination (move/rename a file)
+19. mkdir directory_name (make a new directory)
+20. rmdir directory_name (remove an empty directory)
+
+---------------system information-------------
+
+21. whoami (return the username)
+22. hostname (returns the machine’s network name)
+23. uname -a (gives detailed info about the operating system)
+24. free -m (lists memory usage)
+25. df -h (lists disk space usage on all mounted filesystems)
+
+---------------processes & jobs---------------
+
+26. ps aux (lists all running processes)
+27. top (displays running processes in real time)
+28. kill processID (kill a specific process)
+29. bg (resume a stopped background job)
+30. fg (bring a background job to the foreground)
+
+
+=end
