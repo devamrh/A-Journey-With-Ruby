d1bb56f68fb7d153e84543a62da8611ed37b5c68
A. M. Rafinul Huq <amrh.cse@gmail.com>
2024-02-17 15:59:00 +0600

    2nd file inserted

---
+++ day-02.rb
@@ -0,0 +1,16 @@
+puts "Hello Day 2"
+
+# escape characters
+puts "This is a demo for '\escape\' "
+
+# Puts method
+puts "This is a puts method"
+
+# print vs puts
+
+print "This is print method, it doesn't have new line feature"
+puts "And this is the next line of text"
+
+# the P method
+
+P "The p method is used for just to print the line exactly as it is"
